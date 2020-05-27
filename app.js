2533e4b61dd38da46dace599f7f9456c12734ce3
Douglas de Souza Luz <dougs.2210@gmail.com>
2020-05-26 22:23:08 -0300

    created a modal to show the products details

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ const vm = new Vue({
       fetch(`./api/produtos/${id}/dados.json`)
         .then(r => r.json())
         .then(r => (this.produto = r))
+    },
+    abrirModal (id) {
+      this.fetchProduto(id)
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal ({ target, currentTarget }) {
+      if (target === currentTarget) this.produto = false
     }
   },
   filters: {
