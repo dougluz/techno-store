506ef416de5b7036f602516911bdde5ec178bb84
Douglas de Souza Luz <dougs.2210@gmail.com>
2020-05-27 21:57:34 -0300

    created routes (with functions) and a modal alert

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
-    carrinho: []
+    carrinho: [],
+    mensagemAlerta: 'Item Adicionado',
+    alertaAtivo: false
   },
   methods: {
     fetchProdutos () {
@@ -32,6 +34,7 @@ const vm = new Vue({
       this.produto.estoque--
       const { id, nome, preco } = this.produto
       this.carrinho.push({ id, nome, preco })
+      this.alerta(`${nome} foi adicionado ao carrinho`)
     },
     removerItem (index) {
       this.carrinho.splice(index)
@@ -39,6 +42,19 @@ const vm = new Vue({
     checarLocalStorage () {
       if (window.localStorage.carrinho) {
         this.carrinho = JSON.parse(window.localStorage.carrinho)
+      }
+    },
+    alerta (mensagem) {
+      this.mensagemAlerta = mensagem
+      this.alertaAtivo = true
+      setTimeout(() => {
+        this.alertaAtivo = false
+      }, 1500)
+    },
+    router () {
+      const hash = document.location.hash
+      if (hash) {
+        this.fetchProduto(hash.replace('#', ''))
       }
     }
   },
@@ -57,6 +73,11 @@ const vm = new Vue({
   watch: {
     carrinho () {
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
+    },
+    produto () {
+      document.title = this.produto.nome || 'Techno'
+      const hash = this.produto.id || ''
+      history.pushState(null, null, `#${hash}`)
     }
   },
   filters: {
@@ -67,5 +88,6 @@ const vm = new Vue({
   created () {
     this.fetchProdutos()
     this.checarLocalStorage()
+    this.router()
   }
 })
